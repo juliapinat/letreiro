2c4195b002629f895c0c48aaf003df89ee864589
juliapinat <julia.pinat@escola.pr.gov.br>
2025-11-14 11:58:59 -0300

    ok

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
 let letreiro = document.getElementById("letreiro");
 let tela = document.getElementById("tela");
 
-let posX = 0;                        // posição inicial (esquerda → direita)
-let direcao = 1;                     // 1 = direita, -1 = esquerda
+let posX = 0;                 // posição inicial
+let direcao = 1;              // 1 = direita, -1 = esquerda
+let velocidade = 2;           // velocidade
 
-let velocidade = 2;                  // controle de VELOCIDADE (px por frame)
-
-/* Função de animação */
 function animar() {
 
     posX += direcao * velocidade;
     letreiro.style.left = posX + "px";
 
-    /* Limites de movimento */
     let limiteDireita = tela.clientWidth - letreiro.clientWidth;
 
     if (posX >= limiteDireita) {
-        direcao = -1;               // volta para a esquerda
+        direcao = -1;
     }
 
     if (posX <= 0) {
-        direcao = 1;                // volta para a direita
+        direcao = 1;
     }
 
     requestAnimationFrame(animar);
 }
 
-/* Iniciar animação */
 animar();
+
